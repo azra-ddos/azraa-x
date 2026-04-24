760efba45742e29980715ef9daf709d81647f716
azra cy <mhd.naufalazraduha@gmail.com>
2026-04-24 18:32:35 +0700

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "chat-app-xxx.firebaseapp.com",
-  projectId: "chat-app-xxx",
-  storageBucket: "chat-app-xxx.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:xxxxxxxxxxxx"
+  apiKey: "AIzaSyB6ipWbdqkoE37gdiAJzvOUpn-k4t5uRiU",
+  authDomain: "azra-chat-54758.firebaseapp.com",
+  projectId: "azra-chat-54758",
+  storageBucket: "azra-chat-54758.firebasestorage.app",
+  messagingSenderId: "548218898310",
+  appId: "1:548218898310:web:066caf04d438779bce0e07"
 };
 
 firebase.initializeApp(firebaseConfig);
